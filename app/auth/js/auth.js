dd3c338bdaff89759bbd414f35c61e6bcca01d70
Xpensive-tech <divinep091@gmail.com>
2025-05-23 20:51:08 +0100

    initial commit

--- app/auth/js/auth.js
+++ app/auth/js/auth.js
@@ -12,16 +12,13 @@
 
     const password = document.getElementById("password");
     const eyeIcon = document.getElementById("eyeIcon");
- function togglePassword() {
 
-    if(password.type === "password"){
-        password.type ="text"
-        eyeIcon.src = "../../assets/svg/eye-open-svgrepo-com.svg"
-        eyeIcon.alt = "Hide Password"
-    }else{
-        password.type ="password";
-        eyeIcon.src = "../../assets/svg/eye-closed-duotone-svgrepo-com.svg";
-    }
-}
-
-togglePassword();
+    eyeIcon.addEventListener("click", function(){
+        if(password.type ==="password"){
+            password.type = "text";
+            eyeIcon.src ="../../assets/svg/eye-open-svgrepo-com.svg"
+        }else{
+            password.type = "password";
+            eyeIcon.src ="../../assets/svg/eye-closed-duotone-svgrepo-com.svg"
+        }
+    })
